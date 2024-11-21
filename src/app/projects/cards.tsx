6cd1b9f7e21b3203fe684cf33a1f4d274aa626b5
Matheus Tiburcio <matheusgtiburcio@gmail.com>
2024-11-21 16:18:15 -0300

    upgrade project links

--- src/app/projects/cards.tsx
+++ src/app/projects/cards.tsx
@@ -3,6 +3,7 @@ import PizzaShop from "@/assets/thumbnails/pizzaShop.png";
 import InOrbit from "@/assets/thumbnails/inOrbit.png";
 import Planner from "@/assets/thumbnails/planner.png";
 import ToDoApp from "@/assets/thumbnails/toDoApp.png";
+import { notes, pizzaShop, inOrbit, planner, toDoApp } from "./links.json";
 import { Card } from "./card";
 
 export function Cards() {
@@ -14,8 +15,8 @@ export function Cards() {
           description="Expert Notes é um aplicativo de notas que permite ao usuário criar e gerenciar notas de forma simples e eficiente."
           src={Notes}
           alt="Notes Project"
-          demo="https://nlwnotes.vercel.app/"
-          repository="https://github.com/0kira-vgl/nlwNotes"
+          demo={notes.demo}
+          repository={notes.repository}
         />
 
         <Card
@@ -23,8 +24,8 @@ export function Cards() {
           description="Expert Notes é um aplicativo de notas que permite ao usuário criar e gerenciar notas de forma simples e eficiente."
           src={PizzaShop}
           alt="PizzaShop Project Project"
-          demo="https://github.com/0kira-vgl"
-          repository="https://github.com/0kira-vgl/pizzashop"
+          demo={pizzaShop.demo}
+          repository={pizzaShop.repository}
         />
       </div>
 
@@ -34,8 +35,8 @@ export function Cards() {
           description="O projeto é um site desktop de registro de metas com progresso semanal."
           src={InOrbit}
           alt="InOrbit Project"
-          demo="https://inorbit-two.vercel.app/"
-          repository="https://github.com/0kira-vgl/inorbit"
+          demo={inOrbit.demo}
+          repository={inOrbit.repository}
         />
 
         <Card
@@ -43,8 +44,8 @@ export function Cards() {
           description="O projeto é um site desktop para montar planos de viagem com amigos, registrar atividades e links úteis."
           src={Planner}
           alt="Planner Project"
-          demo="https://github.com/0kira-vgl"
-          repository="https://github.com/0kira-vgl/planner"
+          demo={planner.demo}
+          repository={planner.repository}
         />
 
         <Card
@@ -52,8 +53,8 @@ export function Cards() {
           description="Uma de lista de tarefas onde os usuários podem adicionar, marcar como concluídas e excluir tarefas."
           src={ToDoApp}
           alt="ToDoApp Project"
-          demo="https://to-do-list-0kira-vgl.vercel.app"
-          repository="https://github.com/0kira-vgl/toDoList"
+          demo={toDoApp.demo}
+          repository={toDoApp.repository}
         />
       </div>
     </main>
